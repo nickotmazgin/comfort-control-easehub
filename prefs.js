70e8247f4749d2fb640b7e3cc77c028c59fba089
Nick Otmazgin <nickotmazgin@gmail.com>
2025-09-18 20:21:14 +0300

    chore: Sync with officially approved EGO version (v9.3)

--- prefs.js
+++ prefs.js
@@ -1,15 +1,37 @@
 'use strict';
+
 const { Adw, Gtk } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
-function fillPreferencesWindow(window) {
-  const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.easehub');
-  const page = new Adw.PreferencesPage();
-  const group = new Adw.PreferencesGroup({ title: 'General' });
-  const rowConfirm = new Adw.ActionRow({ title: 'Confirm dangerous actions' });
-  const sw = new Gtk.Switch({ active: settings.get_boolean('confirm-dangerous'), valign: Gtk.Align.CENTER });
-  rowConfirm.add_suffix(sw); rowConfirm.activatable_widget = sw;
-  sw.connect('notify::active', () => settings.set_boolean('confirm-dangerous', sw.active));
-  group.add(rowConfirm); page.add(group); window.add(page);
+
+function init() {
+    // Left empty intentionally
 }
-function init() {}
-function buildPrefsWidget() { const win = new Adw.PreferencesWindow(); fillPreferencesWindow(win); return win; }
+
+function fillPreferencesWindow(window) {
+    const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.easehub');
+    const page = new Adw.PreferencesPage();
+    const group = new Adw.PreferencesGroup({ title: 'General' });
+
+    const rowConfirm = new Adw.ActionRow({ title: 'Confirm dangerous actions' });
+    const sw = new Gtk.Switch({
+        active: settings.get_boolean('confirm-dangerous'),
+        valign: Gtk.Align.CENTER,
+    });
+
+    rowConfirm.add_suffix(sw);
+    rowConfirm.activatable_widget = sw;
+
+    sw.connect('notify::active', () => {
+        settings.set_boolean('confirm-dangerous', sw.active);
+    });
+
+    group.add(rowConfirm);
+    page.add(group);
+    window.add(page);
+}
+
+function buildPrefsWidget() {
+    const window = new Adw.PreferencesWindow();
+    fillPreferencesWindow(window);
+    return window;
+}
